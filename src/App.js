defd47c080d1ff3a01a5b0c9d0e692debe185d05
franchesco famous <franchescofamous@gmail.com>
2023-08-18 20:04:26 +0000

    Reconfigure state , from app component and passed from props to menu, and set food registration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Menu from "./Components/Pages/Menu/Menu";
-
+import Register from "./Components/Pages/Register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 const App = () => {
+  let [mealList, setMealList] = useState([
+    {
+      nom: "spagheti Bolognaise",
+      description: "oate viande haché",
+      prix: 3500,
+    },
+    { nom: "Pizza", description: "fromage,viande hachée", prix: 5000 },
+    { nom: "Burger", description: "fromage,viande", prix: 1500 },
+    {
+      nom: "frites Au Poulet",
+      description: "pomme de terre, poulet",
+      prix: 3500,
+    },
+  ]);
+
   return (
-    <div>
-      <Menu />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Register setMealList={setMealList} mealList={mealList} />}
+        />
+        <Route path="/menu" element={<Menu mealList={mealList} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/Components/Pages/Menu/Menu.jsx
+++ src/Components/Pages/Menu/Menu.jsx
@@ -1,21 +1,7 @@
 import { useState } from "react";
 import style from "./Menu.module.css";
 
-const Menu = () => {
-  let [mealList, setMealList] = useState([
-    {
-      nom: "spagheti Bolognaise",
-      description: "oate viande haché",
-      prix: 3500,
-    },
-    { nom: "Pizza", description: "fromage,viande hachée", prix: 5000 },
-    { nom: "Burger", description: "fromage,viande", prix: 1500 },
-    {
-      nom: "frites Au Poulet",
-      description: "pomme de terre, poulet",
-      prix: 3500,
-    },
-  ]);
+const Menu = ({ mealList }) => {
   let [drinkList, setList] = useState([
     { nom: "Coca", prix: 1000 },
     { nom: "sprite", prix: 1000 },
@@ -41,7 +27,7 @@ const Menu = () => {
   });
   let myDrinkList = drinkList.map((drink, indice) => {
     return (
-      <article className={style.drinks__card}>
+      <article className={style.drinks__card} key={indice}>
         <aside className={style.drinks__card__image}></aside>
         <aside>
           <span>Nom: {drink.nom} </span>
